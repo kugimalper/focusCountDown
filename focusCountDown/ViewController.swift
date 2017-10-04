506d9698d93aed0ffa37e26eeb9f67a6a7f4fcf9
ST5 <st5@ST5-iMac.local>
2017-10-04 10:16:08 +0300

    timer ready

--- focusCountDown/ViewController.swift
+++ focusCountDown/ViewController.swift
@@ -9,7 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// saniye bilgisi
+    var second = 0
+    var timer = Timer()
+    
+    @IBOutlet weak var actionviewsOutlet: UIView!
+    @IBOutlet weak var countDownLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var stepperOutlet: UIStepper!
+    
+    
+    @IBAction func stepperAction(_ sender: UIStepper) {
+        countDownLabel.text! = String (Int(sender.value))
+        print (sender.value)
+        progressBar.progress = 0
+     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print ("ekrana dokundun")
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timerFunc), userInfo: nil, repeats: true)
+    }
+    
+    
+    func timerFunc () {
+        print ("çalışıyor")
+        if second <= 0 {
+            timer.invalidate()
+        }
+        second -= 1
+        print (second)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+     print ("touch ended")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
